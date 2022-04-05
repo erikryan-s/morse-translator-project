acd4db00a467ce0f426b924cf981cdf452c9a7c8
erikryan-s <100544923+erikryan-s@users.noreply.github.com>
2022-04-05 11:25:15 +1000

    Update changeLang.js

--- js/modules/changeLang.js
+++ js/modules/changeLang.js
@@ -1,6 +1,6 @@
 export const changeLang = (objectToSwap) => {
     const langObject = {};
-    // change for each key in the object
+    // change for each key in the object to decode morse code
     Object.keys(objectToSwap).forEach((key) => {
         langObject[objectToSwap[key]] = key;
     });
